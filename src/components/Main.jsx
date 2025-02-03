c8158a8d55ef43659878b1fd434e116aaacfbe7e
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-03 13:12:52 +0100

    feat: add Main and Article components and articles data

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import articlesData from "../data/articlesData";
+import Article from "./Article";
+
+export default function Main() {
+  return (
+    <main>
+      <section className="container"></section>
+    </main>
+  );
+}
---
+++ src/data/articlesData.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    titolo: "Gestire il tempo",
+  },
+  {
+    id: 2,
+    titolo: "Strumenti per il remoto",
+  },
+  {
+    id: 3,
+    titolo: "Meditazione base",
+  },
+];
